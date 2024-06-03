4efc75b261a5668c61607e7cae6c099d1e1a6109
shadow-slave <ashwinsunil02@gmail.com>
2024-06-03 18:49:00 +0530

    nvercel

--- PDesigns/settings.py
+++ PDesigns/settings.py
@@ -29,7 +29,7 @@ with open(os.path.join(BASE_DIR,'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
